cf5ecebb43bf0ebc5c01eca3f6bfd506fafe6c55
Shayanpj <alizade.shayan1988@gmail.com>
2025-04-25 19:18:52 +0330

    change todos title

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -89,7 +89,7 @@ export default function Todos() {
         <div className="flex items-center justify-center h-screen">
             <div className="w-full px-4 py-8 mx-auto shadow lg:w-1/3  bg-white">
                 <div className="flex items-center mb-6">
-                    <h1 className="mr-6 text-4xl font-bold text-purple-600"> Shayan TO DO APP</h1>
+                    <h1 className="mr-6 text-4xl font-bold text-purple-600">Shayan TO DO APP.</h1>
                 </div>
                 <div className="relative">
                     <input type="text" placeholder="What needs to be done today?"
